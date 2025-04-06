5da7a4a544bc6eaab9439eed39806beea42e059a
Ricky Parte <77445318+RickyParte@users.noreply.github.com>
2025-04-06 17:44:51 +0530

    Update index.ts

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -5,7 +5,7 @@ dotenv.config({ path: path.join((process.cwd(), '.env')) });
 
 export default {
   nodeEnv: process.env.NODE_ENV,
-  port: 3000,
+  port: 8000,
   database_url: 'mongodb+srv://sanketwalhekar83:9665998329@cluster0.sevwc.mongodb.net/invetory',
   jwt_secret: 'eyjHbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9'
 };
